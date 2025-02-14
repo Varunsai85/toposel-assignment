434892466e99e7681ed369f694bc1f1f64055998
Varun Sai Nadiminti <101313544+Varunsai85@users.noreply.github.com>
2025-02-14 15:00:14 +0530

    V1.0

--- controllers/server.controller.js
+++ controllers/server.controller.js
@@ -10,13 +10,13 @@ const generateToken = (userId) => {
 };
 
 const setCookie = (res, accessToken) => {
-    res.cookie("accesToken", accessToken, {
+    res.cookie("accessToken", accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: true,
         maxAge: 15 * 60 * 1000
     })
-}
+};
 
 export const userSignUp = async (req, res) => {
     const { fullName, userName, email, password, dateOfBirth, gender, country } = req.body;
@@ -26,9 +26,47 @@ export const userSignUp = async (req, res) => {
             return res.status(400).json({ message: "User already exists" });
         };
 
+        if (!email || !fullName || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        if (password.length < 6) return res.status(400).json({ message: "Password must have 6 characters" });
+
         const user = await User.create({ fullName, userName, email, password, dateOfBirth, gender, country });
         const accessToken = generateToken(user._id);
-        setCookie(res,accessToken);
+        setCookie(res, accessToken);
+        res.status(201).json({
+            user: {
+                _id: user._id,
+                fullName: user.fullName,
+                userName: user.userName,
+                email: user.email
+            },
+            message: "User created successfully"
+        })
+    } catch (error) {
+        console.log(`Error in userSignUp controller`, error.message)
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+export const userLogIn = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User Not Found" });
+        };
+
+        const isPassword = await user.comparePassword(password);
+        if (!isPassword) {
+            return res.status(400).json({ message: "Incorrect Credentials" });
+        };
+
+
+        const accessToken = generateToken(user._id);
+        setCookie(res, accessToken);
+
         res.status(200).json({
             user: {
                 _id: user._id,
@@ -36,9 +74,43 @@ export const userSignUp = async (req, res) => {
                 userName: user.userName,
                 email: user.email
             },
-            message: "user created successfully"
+            message: "User login successfully"
         })
     } catch (error) {
+        console.log(`Error in userLogIn controller`, error.message)
         res.status(500).json({ message: error.message });
     }
 };
+
+export const userSearch = async (req, res) => {
+    const { userName } = req.body;
+    try {
+        const user = await User.findOne({ userName });
+        if (!user) {
+            return res.status(404).json({ message: "User Not Found" });
+        };
+
+        res.status(200).json({
+            user: {
+                _id: user._id,
+                fullName: user.fullName,
+                userName: user.userName,
+                email: user.email
+            },
+            message: "User Found"
+        })
+    } catch (error) {
+        console.log(`Error in userSearch controller`, error.message)
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const userLogOut = async (req, res) => {
+    try {
+        res.clearCookie("accessToken");
+        res.json({message:"User LoggedOut Successfully"})
+    } catch (error) {
+        console.log(`Error in userLogOut controller`, error.message)
+        res.status(500).json({ message: error.message });
+    }
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
-import { userSignUp } from "./controllers/server.controller.js";
+import { userLogIn, userLogOut, userSearch, userSignUp } from "./controllers/server.controller.js";
 const app=express();
 
 dotenv.config();
@@ -15,7 +15,10 @@ app.get("/",(req,res)=>{
     res.send("Toposel Assignment")
 });
 
-app.post("/user-registration",userSignUp);
+app.post("/signUp",userSignUp);
+app.post("/logIn",userLogIn);
+app.post("/userSearch",userSearch);
+app.post("/logOut",userLogOut)
 
 app.listen(PORT,()=>{
     console.log(`Listening on http://localhost:${PORT}`);
